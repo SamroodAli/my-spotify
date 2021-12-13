39666b54119497e650778f6057f5f67ee503fc5e
samroodAli <devsamrood@gmail.com>
2021-12-13 22:22:16 +0530

    Css:Import reset css package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
