b4296cadb53a30115a7afad139188a64abd0ddba
samroodAli <devsamrood@gmail.com>
2021-12-13 22:35:42 +0530

    Layout:Add player layout to app pages with _app and pageLayout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      layout
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "reset-css";
+import PlayerLayout from "../components/playerLayout";
 
 const theme = extendTheme({
   colors: {
@@ -32,7 +33,9 @@ const theme = extendTheme({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <PlayerLayout>
+        <Component {...pageProps} />;
+      </PlayerLayout>
     </ChakraProvider>
   );
 };
