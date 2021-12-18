f772145794b33e5041e5cddacc206b6693848a4f
samroodAli <devsamrood@gmail.com>
2021-12-18 16:25:59 +0530

    Seed:Create user and associate playlists and songs with the user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,6 +23,35 @@ const run = async () => {
       });
     })
   );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("password", salt),
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map((_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
