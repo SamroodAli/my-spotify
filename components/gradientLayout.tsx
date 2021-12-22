21dd9789e2dc1208bf6ed7f140285f08b5c15f36
samroodAli <devsamrood@gmail.com>
2021-12-22 20:32:38 +0530

    GradientLayout:Create gradientLayout for the page main content with gradient, texts and image

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,42 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
+const Gradientlayout = ({
+  color,
+  children,
+  image,
+  subtitle,
+  title,
+  description,
+  roundImage = false,
+}) => {
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="40px" align="end">
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding="20px" lineHeight="40px" color="#fff">
+          <Text fontSize="sm" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize="x-small" fontWeight="100">
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Gradientlayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import GradientLayout from "../components/gradientLayout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="green"
+      subtitle={"profile"}
+      title="Scott Moss"
+      description={`15 Public playlists`}
+      image="https://dl.dropboxusercontent.com/s/8tyfzyb3b34ouxd/Openhttps://dl.dropboxusercontent.com/s/8tyfzyb3b34ouxd/Open"
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
