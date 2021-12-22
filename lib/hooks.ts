868a2af62d1682f9da5bd7dffc8636360ebf8dde
samroodAli <devsamrood@gmail.com>
2021-12-22 23:56:59 +0530

    Api:Add playlist count to userMe returning json and add it to response type for hooks/ useMe

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,8 +2,12 @@ import { Playlist, User } from "@prisma/client";
 import useSWR from "swr";
 import fetcher from "./fetcher";
 
+interface UserMe extends User {
+  playlistCount: number;
+}
+
 export const useMe = () => {
-  const { data, error } = useSWR<User>("/me", fetcher);
+  const { data, error } = useSWR<UserMe>("/me", fetcher);
 
   return {
     user: data,
