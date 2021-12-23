c24ee079a73140f4cce2d9ecfd06ac755468f9c8
samroodAli <devsamrood@gmail.com>
2021-12-23 11:42:15 +0530

    State:Provide store to app usign StoreProvider from easy peasy in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import PlayerLayout from "../components/playerLayout";
+import { StoreProvider } from "easy-peasy";
+import { store } from "../lib/store";
 import "reset-css";
 
 const theme = extendTheme({
@@ -33,13 +35,15 @@ const theme = extendTheme({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      {Component.authPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <PlayerLayout>
+      <StoreProvider store={store}>
+        {Component.authPage ? (
           <Component {...pageProps} />
-        </PlayerLayout>
-      )}
+        ) : (
+          <PlayerLayout>
+            <Component {...pageProps} />
+          </PlayerLayout>
+        )}
+      </StoreProvider>
     </ChakraProvider>
   );
 };
