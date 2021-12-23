7d0138cf65bcb3de427d0ee48a95bbd0e6886d8c
samroodAli <devsamrood@gmail.com>
2021-12-23 10:28:57 +0530

    Playlist:Error handling if token verification failed

--- pages/playlists/[id].tsx
+++ pages/playlists/[id].tsx
@@ -42,11 +42,23 @@ const PlaylistShow = ({ playlist }: { playlist: PlaylistWithSongs }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN!);
+  let user;
+
+  try {
+    const user = validateToken(req.cookies.TRAX_ACCESS_TOKEN!);
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/signin",
+      },
+    };
+  }
+
   const playlist = await prisma.playlist.findFirst({
     where: {
       id: Number(query.id),
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
