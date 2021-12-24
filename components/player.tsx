d65c7ca24d03c0fffeabd0f97c83b8e50136c770
samroodAli <devsamrood@gmail.com>
2021-12-24 19:41:20 +0530

    Player:Change player current song playing position with the seekbar

--- components/player.tsx
+++ components/player.tsx
@@ -26,9 +26,11 @@ const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
   const [index, setIndex] = useState(0);
   const [seek, setSeek] = useState(0.0);
+  const [isSeeking, setIsSeeking] = useState(false);
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
   const [duration, setDuration] = useState(0.0);
+  const soundRef = useRef(null);
 
   const setPlayState = (value) => {
     setPlaying(value);
@@ -37,14 +39,62 @@ const Player = ({ songs, activeSong }) => {
   const onShuffle = () => {
     setShuffle((prevState) => !prevState);
   };
+
   const onRepeat = () => {
     setRepeat((prevState) => !prevState);
+  };
+
+  const prevSong = () => {
+    setIndex((state) => {
+      // if current song song is the first song, return the last song as previous, else return the song before the current song
+      // shuffle logic is not implemented for previous song
+      return state === 0 ? songs.length - 1 : state - 1;
+    });
+  };
+
+  const nextSong = () => {
+    const nextSongIndex = (prevSongIndex: number) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length);
+        if (next === prevSongIndex) {
+          return nextSongIndex(prevSongIndex);
+        }
+        return next;
+      }
+      return prevSongIndex === songs.length - 1 ? 0 : prevSongIndex + 1;
+    };
+
+    setIndex((prevSongIndex) => nextSongIndex(prevSongIndex));
+  };
+
+  const onEnd = () => {
+    if (repeat) {
+      soundRef.current.seek(0); // repeat song by starting again from the beginning if repeat is on
+    } else {
+      nextSong();
+    }
+  };
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration(); // get current activesong duration
+    setDuration(songDuration); // update ui with song duration
+  };
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0])); // update ui //rangeSlider gives u an array with min,max, the min is where the user dragged the seek bar to
+    soundRef.current.seek(e[0]); // update playing song using react howler ref
   };
 
   return (
     <Box>
       <Box>
-        <ReactHowler playing={playing} src={activeSong?.url} />
+        <ReactHowler
+          ref={soundRef}
+          playing={playing}
+          src={activeSong?.url}
+          onLoad={onLoad}
+          onEnd={onEnd}
+        />
       </Box>
       <Center color="gray.600">
         <ButtonGroup>
@@ -63,6 +113,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="previous"
             fontSize="25px"
             icon={<MdSkipPrevious />}
+            onClick={prevSong}
           />
           {playing ? (
             <IconButton
@@ -92,6 +143,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="next"
             fontSize="24px"
             icon={<MdSkipNext />}
+            onClick={nextSong}
           />
           <IconButton
             outline="none"
@@ -113,10 +165,13 @@ const Player = ({ songs, activeSong }) => {
             <RangeSlider
               aria-label={["min", "max"]} // eslint-disable-line jsx-a11y/aria-proptypes
               step={0.1}
+              id="player-range"
               min={0}
-              max={321}
-              id=""
-              player-range
+              max={duration || 0}
+              value={[seek]} // pass as min the current seek value to move the seek bar
+              onChange={onSeek}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
             >
               <RangeSliderTrack bg="gray.800">
                 <RangeSliderFilledTrack bg="gray.600" />
